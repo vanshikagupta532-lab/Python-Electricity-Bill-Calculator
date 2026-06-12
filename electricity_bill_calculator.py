6668d43ce72bf09cebb7f4b44dbeff109631cb6c
Vanshika Gupta <vanshikagupta532@gmail.com>
2026-06-12 17:56:33 +0530

    Enhance electricity bill calculator with customer infoImproved Electricity Bill Calculator with customer details and billing summary
    Added customer details and fixed charge to the bill calculation.

--- electricity_bill_calculator.py
+++ electricity_bill_calculator.py
@@ -1,23 +1,37 @@
-print("===== Electricity Bill Calculator =====")
+print("===== ELECTRICITY BILL CALCULATOR =====")
 
-units = int(input("Enter electricity units consumed: "))
+customer_name = input("Enter Customer Name: ")
+meter_number = input("Enter Meter Number: ")
 
+units = int(input("Enter Units Consumed: "))
+
+# Energy Charge Calculation
 if units <= 100:
-    bill = units * 5
+    energy_charge = units * 5
 
 elif units <= 200:
-    bill = (100 * 5) + ((units - 100) * 7)
+    energy_charge = (100 * 5) + ((units - 100) * 7)
 
 else:
-    bill = (100 * 5) + (100 * 7) + ((units - 200) * 10)
+    energy_charge = (100 * 5) + (100 * 7) + ((units - 200) * 10)
 
-print("\nElectricity Units:", units)
-print("Total Bill: ₹", bill)
-if bill < 500:
-    print("Category: Low Consumption")
+fixed_charge = 100
 
-elif bill < 1500:
-    print("Category: Moderate Consumption")
+total_bill = energy_charge + fixed_charge
 
+# Category
+if units <= 100:
+    category = "Low Consumption"
+elif units <= 200:
+    category = "Moderate Consumption"
 else:
-    print("Category: High Consumption")
+    category = "High Consumption"
+
+print("\n===== BILL SUMMARY =====")
+print("Customer Name :", customer_name)
+print("Meter Number  :", meter_number)
+print("Units Consumed:", units)
+print("Energy Charge : ₹", energy_charge)
+print("Fixed Charge  : ₹", fixed_charge)
+print("Total Bill    : ₹", total_bill)
+print("Category      :", category)
